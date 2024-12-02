d76afd450f54c5109f17712d8b263589a22cc6c2
tobiaskoenig2106 <71609356+tobiaskoenig2106@users.noreply.github.com>
2024-12-02 21:31:03 +0100

    Start of the AES-CBC-Encryption Implementation

--- Implementation/Arduino/arduino_bluetooth/arduino_bluetooth.ino
+++ Implementation/Arduino/arduino_bluetooth/arduino_bluetooth.ino
@@ -1,4 +1,7 @@
 #include <ArduinoBLE.h>
+#include <string>
+#include <string.h>
+#include <AESLib.h>
 #include "MICS-VZ-89TE.h"
 
 MICS_VZ_89TE CO2SENSOR;
@@ -19,6 +22,14 @@ BLEService sensorService("12345678-1234-5678-1234-56789abcdef0");
 BLEIntCharacteristic co2Characteristic("87654321-4321-6789-4321-fedcba987654", BLERead | BLENotify);
 BLEIntCharacteristic vocCharacteristic("abcdef01-2345-6789-0123-456789abcdef", BLERead | BLENotify);
 
+//AES Keys
+AESLib aes;
+byte aes_key[16] = {0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36};
+byte aes_iv[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+char cleartext[32] = {0};
+char ciphertext[64] = {0};
+byte enc_iv_to[N_BLOCK] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+byte enc_iv_from[N_BLOCK] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 // Timing
 const long interval = 10000; // 10 seconds
@@ -26,6 +37,25 @@ long timestamp = 0;
 
 unsigned long noCentralPreviousMillis = 0;
 const long noCentralInterval = 1000; // 1 second for BLE advertising
+
+uint16_t encrypt_to_ciphertext(char * msg, byte iv[]) {
+  int msgLen = strlen(msg);
+  int cipherlength = aes.get_cipher64_length(msgLen);
+  char encrypted_bytes[cipherlength];
+  uint16_t enc_length = aes.encrypt64((byte*)msg, msgLen, encrypted_bytes, aes_key, sizeof(aes_key), iv);
+  sprintf(ciphertext, "%s", encrypted_bytes);
+  return enc_length;
+}
+
+void decrypt_to_cleartext(char * msg, uint16_t msgLen, byte iv[]) {
+  Serial.println("Calling decrypt64...");
+#ifdef ESP8266
+  Serial.print("[decrypt_to_cleartext] free heap: "); Serial.println(ESP.getFreeHeap());
+#endif
+  uint16_t decLen = aes.decrypt64(msg, msgLen, (byte*)cleartext, aes_key, sizeof(aes_key), iv);
+  Serial.print("Decrypted bytes: "); Serial.println(decLen);
+}
+
 
 void setup() {
     Serial.begin(57600);
@@ -52,6 +82,18 @@ void setup() {
 
     BLE.advertise();
     Serial.println("BLE advertising and sensor started...");
+
+
+    memcpy(enc_iv_to, aes_iv, sizeof(aes_iv));
+    memcpy(enc_iv_from, aes_iv, sizeof(aes_iv));
+    strncpy(cleartext, "Hallo, Welt!", sizeof(cleartext) - 1);
+    Serial.println(cleartext);
+    uint16_t len = encrypt_to_ciphertext(cleartext, enc_iv_to);
+    Serial.println(ciphertext);
+    Serial.println("Encrypted. Decrypting..."); Serial.flush();
+    decrypt_to_cleartext(ciphertext, len, enc_iv_from);
+    Serial.println(cleartext);
+
 }
 
 void loop() {
