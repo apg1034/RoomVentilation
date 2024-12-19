60648504043311036a2e4e7cca28035574bed0da
Timur Ali <138223486+ryderX95@users.noreply.github.com>
2024-12-19 22:29:47 +0100

    Update action_control.h
    Merged

--- Implementation/Raspi/action_control.h
+++ Implementation/Raspi/action_control.h
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 void setAction(int value);
 void actionOpen();
 void actionClose();
@@ -8,14 +6,3 @@ void closeWindow();
 void runVentilation();
 void stopVentilation();
 void calibrateWindowPosition();
-
-=======
-
-void setAction(int value);
-void actionOpen();
-void actionClose();
-void openWindow();
-void closeWindow();
-void runVentilation();
-void stopVentilation();
->>>>>>> adea0023f5c375dfcd32ecb78233ae5518a6c95d
